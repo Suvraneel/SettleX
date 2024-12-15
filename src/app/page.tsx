0f0ee5af8cbe46ddffceb9571e58886d733af5cc
Suvraneel Bhuin <bsuvraneel@gmail.com>
2024-12-16 03:30:57 +0530

    init(landing page v0.1): hero-section & spline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,83 @@
-import Image from "next/image";
+"use client";
+import {FloatingNav} from "@components/ui/floating-navbar";
+import {ThemeProvider} from "@components/theme-provider";
+import {BrandGithub, BrandSlack, BrandTelegram, BrandX, ChevronRight} from "@mynaui/icons-react";
+import Spline from "@splinetool/react-spline";
+
+const navItems = [
+    {name: "X", link: "/", icon: <BrandX className="h-5 w-5"/>},
+    {name: "Telegram", link: "/", icon: <BrandTelegram className="h-5 w-5"/>},
+    {name: "Github", link: "/", icon: <BrandGithub className="h-5 w-5"/>},
+    {name: "Slack", link: "/", icon: <BrandSlack className="h-5 w-5"/>},
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div
+            className="h-screen overflow-hidden grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col relative gap-8 row-start-2 items-center sm:items-start">
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+                    <FloatingNav navItems={navItems}/>
+                    <div
+                        className="h-2/3 w-full dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-start">
+                        <div className="w-1/2 flex flex-col">
+                            <span className="text-2xl capitalize text-cyan-300 max-[410px]:text-xl xl:text-4xl"
+                                  aria-label="Encrypted dynamic text">
+                                Encrypted <span className="text-black dark:text-white" aria-live="polite"
+                                                role="text">transfers</span>
+                            </span>
+                            <h1 className="mt-4 text-3xl leading-snug min-[410px]:text-4xl xl:text-6xl 2xl:text-7xl">
+                                The Modular<br/>
+                                <span className="text-black dark:text-white" aria-live="polite" role="text">Interoperability</span><br/>
+                                Hall
+                            </h1>
+                            <p className="mt-4 text-base text-black dark:text-white/70 max-[410px]:text-sm sm:pr-4 xl:text-xl">
+                                SettleX is the first FhEVM Clearing Layer, solving fragmentation in modular blockchains
+                                while providing privacy for users. non-EVM networks.
+                            </p>
+                            <p className="mt-8 flex items-center gap-4 sm:gap-8 text-base text-black dark:text-white/70 max-[410px]:text-sm xl:text-xl"
+                               aria-label="Powered by Union">
+                                Powered by:
+                                <span className="flex items-center gap-4 sm:gap-8">
+                                    <a href="https://union.build" rel="noopener noreferrer" target="_blank"
+                                       className="group">
+                                        <img alt="Union - A Fluton Partner" draggable="false" loading="lazy" width="656"
+                                             height="182" decoding="async" data-nimg="1"
+                                             className="h-6 w-auto select-none group-hover:opacity-70 sm:h-8"
+                                             style={{color: "transparent"}} src="./union-logo-white.svg"/>
+                                    </a>
+                                </span>
+                            </p>
+                            <div className="mt-16 flex items-center gap-8 max-[410px]:gap-2">
+                                <a href="mailto: settlexfoundation@gmail.com" target="_blank" rel="noopener noreferrer"
+                                   aria-label="Contact Us">
+                                    <button
+                                        className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md
+                                        text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
+                                        disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0
+                                        bg-primary hover:bg-zinc-700 text-primary-foreground shadow hover:bg-primary/70 px-4 py-2 h-[38px]">
+                                        Contact Us
+                                    </button>
+                                </a>
+                                <a aria-label="Join the waitlist" href="/join-waitlist">
+                                    <button
+                                        className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md
+                                        text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
+                                        disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 group
+                                        bg-transparent shadow border border-cyan-300 text-black dark:text-white hover:bg-accent/30 hover:text-neutral-500 h-9 px-4 py-2">
+                                        Join Waitlist<ChevronRight className="h-4 w-4 group-hover:translate-x-1 transition-transform"/>
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="w-7/12 absolute right-0 h-[80vh] flex items-center justify-center">
+                            <Spline
+                                scene="https://prod.spline.design/L9ko-MFfr72kdSnS/scene.splinecode"
+                            />
+                        </div>
+                    </div>
+                </ThemeProvider>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
